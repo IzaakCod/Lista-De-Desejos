463024a7524161d706137fea23cb636b59c14f7a
IzaakCod <zaaknascimentosantos@gmail.com>
2026-09-09 21:54:00 -0300

    Continuação da uala de CSS2

--- index.js
+++ index.js
@@ -9,9 +9,28 @@ function adicionarItem() {
     const novoItem = document.getElementById("novoItem");
     const itens = document.getElementById("itens");
 
+    let texto = novoItem.value.trim()
+
+
+
+    if (texto == "") {
+        alert("Preencha o campo")
+        return
+    }
+
     itens.innerHTML += `<div class="item">
-                            <span>${novoItem.value}</span>
-                            <button>X</button>
-                        </div>`;
+                            <span>${texto}</span>
+                            <div>
+                                <button>✏️</button>
+                                <button onclick="excluirItem(ev)>🗑️</button>
+                            </div>                       
+                            </div>`;
     novoItem.value = "";
+}
+function excluirItem(ev){
+    console.log(ev)
+
+    const button = ev.target
+    const item = button.parentElement.parentElement
+    item.remove()
 }
