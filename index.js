87a541d7b1e5f5e3d1941bde289d0ab7a4e1ac10
IzaakCod <zaaknascimentosantos@gmail.com>
2026-09-16 21:49:12 -0300

    Aula sobre alguns novos elementos, e sobre localStorage

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
-let usuario = "Izaak"
-//TODO prompt("Informe seu nome");
+const usuario = 'Izaak'; //TODO prompt('Informe seu nome');
 
-let bemVindo = document.getElementById("bemVindo");
+const bemVindo = document.getElementById('bemVindo');
 bemVindo.innerHTML = `Seja bem-vindo(a), ${usuario}!`;
 
+let lista = JSON.parse(localStorage.getItem("lista")) || [];
+exibirLista()
+
+function exibirLista() {
+    const itens = document.getElementById('itens');
+    itens.innerHTML = '';
+
+    for (let i = 0; i < lista.length; i++) {
+        itens.innerHTML += `<div class='item'>
+                                <span>${lista[i]}</span>
+                                <div>
+                                    <button onclick='editarItem(${i})'>✏️</button>
+                                    <button onclick='excluirItem(${i})'>🗑️</button>
+                                </div>
+                            </div>`;
+    }
+}
 
 function adicionarItem() {
-    const novoItem = document.getElementById("novoItem");
-    const itens = document.getElementById("itens");
+    const novoItem = document.getElementById('novoItem');
 
-    let texto = novoItem.value.trim()
+    let texto = novoItem.value.trim();
 
-
-
-    if (texto == "") {
-        alert("Preencha o campo")
-        return
+    if (texto == '') {
+        alert('Preencha o campo!');
+    } else {
+        lista.push(texto);
+        localStorage.setItem("lista", JSON.stringify(lista))
+        exibirLista();
     }
 
-    itens.innerHTML += `<div class="item">
-                            <span>${texto}</span>
-                            <div>
-                                <button>✏️</button>
-                                <button onclick="excluirItem(ev)>🗑️</button>
-                            </div>                       
-                            </div>`;
-    novoItem.value = "";
+    novoItem.value = '';
 }
-function excluirItem(ev){
-    console.log(ev)
 
-    const button = ev.target
-    const item = button.parentElement.parentElement
-    item.remove()
+function excluirItem(index) {
+    console.log('Excluindo posição: ', index);
+    lista.splice(index, 1);
+    localStorage.setItem("lista", JSON.stringify(lista))
+    exibirLista();
+}
+
+function editarItem(index) {
+    let texto = prompt(`Informe o novo texto para ${lista[index]}`);
+    texto = texto.trim();
+
+    if (texto == '') {
+        alert('Preencha, desgraça!');
+    } else {
+        lista[index] = texto;
+        localStorage.setItem("lista", JSON.stringify(lista))
+        exibirLista();
+    }
 }
